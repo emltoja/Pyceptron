671f35ea08ef8c589ceebaa8ada16e70cb8eeae6
Emil Olszewski <268789@student.pwr.edu.pl>
2023-09-14 17:54:45 +0200

    fixed wrong product. Kronecker -> Hadamard

--- pyceptron.py
+++ pyceptron.py
@@ -13,15 +13,15 @@ class Perceptron:
         learningRate=0.1,
         trainingThreshold=0.1,
     ) -> None:
-        self.dims = (width, height)                         # Dimension of percepted specimens (as well as dimension of weights matrix)
-        self.bias = bias                                    # Bias of the Perceptron
-        self.learningRate = learningRate                    # Learning rate of the Perceptron
-        self.weights = np.zeros((width, height))            # Weights of the Perceptron
-        self.trainingSet = trainingSet                      # Set of tuples (specimen, stype) used for training
-        self.trainingTime = len(trainingSet)                # Length of training set
-        self.trainingThreshold = trainingThreshold          # Boundary for accepting the training results
-        self.currentSpecimen = 0                            # Specimen currently used for training the Perceptron
-        self.accumError = 0                                 # Error accumulated during training
+        self.dims = (width, height)                                    # Dimension of percepted specimens (as well as dimension of weights matrix)
+        self.bias = bias                                               # Bias of the Perceptron
+        self.learningRate = learningRate                               # Learning rate of the Perceptron
+        self.weights = np.zeros((width, height))                       # Weights of the Perceptron
+        self.trainingSet = trainingSet                                 # Set of tuples (specimen, stype) used for training
+        self.trainingTime = len(trainingSet) if trainingSet else 0     # Length of training set
+        self.trainingThreshold = trainingThreshold                     # Boundary for accepting the training results
+        self.currentSpecimen = 0                                       # Specimen currently used for training the Perceptron
+        self.accumError = 0                                            # Error accumulated during training
 
 
     # Restart the perceptron
@@ -30,7 +30,7 @@ class Perceptron:
 
     # Linear output of the neuron
     def threshold(self, input: np.matrix) -> float:
-        return np.sum(np.kron(self.weights, input)) + self.bias
+        return np.sum(np.multiply(self.weights, input)) + self.bias
 
 
     # Train the Perceptron on next specimen in training set
@@ -38,8 +38,10 @@ class Perceptron:
 
         
         sample = self.trainingSet[self.currentSpecimen]
-        output = self.threshold(sample[0]) > 0
-        diff = sample[1] - output
+        specimen = sample[0]
+        desired = sample[1]
+        output = 1 if self.threshold(specimen) > 0 else 0
+        diff = desired - output
         self.accumError += np.abs(diff)
 
         # Update the weights
@@ -52,10 +54,18 @@ class Perceptron:
         # Last specimen in the set. Finish training
         if self.currentSpecimen == self.trainingTime - 1:
             self.currentSpecimen = 0
-            return 0
+            return False
         
         # If the training threshold has been passeed, then finish the training
         if self.accumError > self.trainingTime * self.trainingThreshold:
             return False
         return True
 
+
+    # Evaluate the given input by weights
+    # True - Rectangle
+    # False - Circle
+    def evaluate(self, input) -> bool:
+        result = self.threshold(input)
+        print(result)
+        return result > 0
